e10084a1b4e8b05bac82d8214b269bf9a4b5ac72
kjw11 <kangjw.mail@gmail.com>
2023-12-08 17:19:51 +0800

    update

--- pub.js
+++ pub.js
@@ -5,112 +5,128 @@ var publications = [
         title: 'QS-TTS: Towards Semi-Supervised Text-to-Speech Synthesis via Vector-Quantized Self-Supervised Speech Representation Learning',
         authers: ['Haohan Guo', 'Fenglong Xie', 'Jiawen Kang', 'Yujia Xiao', 'Xixin Wu', 'Helen Meng'],
         pub: 'ArXiv preprint arXiv:2309.00126',
-        time: '2023'
+        time: '2023',
+        highlight: ''
        },
        {
         url: 'https://arxiv.org/pdf/2306.13307.pdf',
         title: 'Towards Effective and Compact Contextual Representation for Conformer Transducer Speech Recognition Systems',
         authers: ['Mingyu Cui', 'Jiawen Kang', 'Jiajun Deng', 'Xi Yin', 'Yutao Xie', 'Xie Chen', 'Xunying Liu'],
-        pub: 'INTERSPEECH 2023 (Oral)',
-        time: '2023'
+        pub: 'INTERSPEECH 2023',
+        time: '2023',
+        highlight: 'Oral'
        },
        {
         url: 'https://www1.se.cuhk.edu.hk/~hccl/publications/pub/Lingwei%20Meng%20-%20Sidecar%20-%20update.pdf',
         title: 'Unified Modeling of Multi-Talker Overlapped Speech Recognition and Diarization with a Sidecar Separator',
         authers: ['Lingwei Meng', 'Jiawen Kang', 'Mingyu Cui', 'Haibin Wu', 'Xixin Wu', 'Helen Meng'],
         pub: 'INTERSPEECH 2023',
-        time: '2023'
+        time: '2023',
+        highlight: ''
        },
        {
         url: 'https://www1.se.cuhk.edu.hk/~hccl/publications/pub/Lingwei%20Meng%20-%20Sidecar%20-%20update.pdf',
         title: 'A Sidecar Separator Can Convert A Single-Talker Speech Recognition System to A Multi-Talker One',
         authers: ['Lingwei Meng', 'Jiawen Kang', 'Mingyu Cui', 'Yuejiao Wang', 'Xixin Wu', 'Helen Meng'],
-        pub: 'ICASSP 2023 (Oral)',
-        time: '2023'
+        pub: 'ICASSP 2023',
+        time: '2023',
+        highlight: 'Oral'
        },
        {
         url: 'https://arxiv.org/pdf/2305.12804.pdf',
         title: 'The defender\'s perspective on automatic speaker verification: An overview',
         authers: ['Haibin Wu', 'Jiawen Kang', 'Lingwei Meng', 'Helen Meng', 'Hung-yi Lee'],
-        pub: 'IJCAI DADA 2023 (Oral)',
-        time: '2023'
+        pub: 'IJCAI DADA 2023',
+        time: '2023',
+        highlight: 'Oral',
        },
        {
         url: 'https://www.isca-speech.org/archive/pdfs/interspeech_2023/meng23d_interspeech.pdf',
         title: 'Integrated and enhanced pipeline system to support spoken language analytics for screening neurocognitive disorders',
         authers: ['Helen Meng', 'Brian Mak', 'Man-Wai Mak', 'Helene Fung', 'Xianmin Gong', 'Timothy Kwok', 'Xunying Liu', 'Vincent Mok', 'Patrick Wong', 'Jean Woo', 'Xixin Wu', 'Ka Ho Wong', 'Sean Shensheng Xu', 'Naijun Zheng', 'Ranzo Huang', 'Jiawen Kang*', 'Xiaoquan Ke', 'Junan Li', 'Jinchao Li', 'Yi Wang'],
         pub: 'INTERSPEECH 2023',
-        time: '2023'
+        time: '2023',
+        highlight: ''
        },
        {
         url: 'https://arxiv.org/pdf/2206.09131.pdf',
         title: 'Tackling spoofing-aware speaker verification with multi-model fusion',
         authers: ['Haibin Wu', 'Jiawen Kang', 'Lingwei Meng', 'Yang Zhang', 'Xixin Wu', 'Zhiyong Wu', 'Hung-yi Lee', 'Helen Meng'],
         pub: 'Odyssey 2022',
-        time: '2022'
+        time: '2022',
+        highlight: ''
        },
        {
         url: 'https://arxiv.org/pdf/2202.01986.pdf',
         title: 'The cuhk-tencent speaker diarization system for the icassp 2022 multi-channel multi-party meeting transcription challenge',
         authers: ['Naijun Zheng', 'Na Li', 'Xixin Wu', 'Lingwei Meng', 'Jiawen Kang', 'Haibin Wu', 'Chao Weng', 'Dan Su', 'Helen Meng'],
         pub: 'ICASSP 2022',
-        time: '2022'
+        time: '2022',
+        highlight: ''
        },
        {
         url: 'https://arxiv.org/pdf/2202.08216.pdf',
         title: 'TalkTive: a conversational agent using backchannels to engage older adults in neurocognitive disorders screening',
         authers: ['Zijian Ding', 'Jiawen Kang', 'Tinky Oi Ting Ho', 'Ka Ho Wong', 'Helene H Fung', 'Helen Meng', 'Xiaojuan Ma'],
-        pub: 'CHI 2022 (Oral)',
-        time: '2022'
+        pub: 'CHI 2022',
+        time: '2022',
+        highlight: 'Oral'
        },
        {
         url: 'https://arxiv.org/pdf/2203.15377.pdf',
         title: 'Spoofing-Aware Speaker Verification by Multi-Level Fusion',
         authers: ['Haibin Wu', 'Lingwei Meng', 'Jiawen Kang', 'Jinchao Li', 'Xu Li', 'Xixin Wu', 'Hung-yi Lee', 'Helen Meng'],
         pub: 'INTERSPEECH 2022',
-        time: '2022'
+        time: '2022',
+        highlight: ''
        },
        {
         url: 'https://arxiv.org/pdf/2012.12471.pdf',
         title: 'A principle solution for enroll-test mismatch in speaker recognition',
         authers: ['Lantian Li', 'Dong Wang', 'Jiawen Kang', 'Renyu Wang', 'Jing Wu', 'Zhendong Gao', 'Xiao Chen'],
         pub: 'IEEE/ACM Transactions on Audio, Speech, and Language Processing',
-        time: '2021'
+        time: '2021',
+        highlight: ''
        },
        {
         url: 'https://arxiv.org/pdf/2012.12468.pdf',
         title: 'Cn-celeb: multi-genre speaker recognition',
         authers: ['Jiawen Kang', 'Lantian Li', 'Ruiqi Liu', 'Yue Fan', 'Hao Cui', 'Yunqi Cai', 'Ravichander Vipperla', 'Thomas Fang Zheng', 'Dong Wang'],
         pub: 'Speech Communication',
-        time: '2021'
+        time: '2021',
+        highlight: ''
        },
        {
         url: 'https://arxiv.org/pdf/2010.14243.pdf',
         title: 'Squeezing value of cross-domain labels: A decoupled scoring approach for speaker verification',
         authers: ['Lantian Li', 'Yang Zhang', 'Jiawen Kang', 'Thomas Fang Zheng', 'Dong Wang'],
         pub: 'ICASSP 2021',
-        time: '2021'
+        time: '2021',
+        highlight: ''
        },
        {
         url: 'https://arxiv.org/pdf/2005.11900.pdf',
         title: 'Domain-invariant speaker vector projection by model-agnostic meta-learning',
         authers: ['Jiawen Kang', 'Ruiqi Liu', 'Lantian Li', 'Yunqi Cai', 'Dong Wang', 'Thomas Fang Zheng'],
-        pub: 'INTERSPEECH 2020 (Oral)',
-        time: '2020'
+        pub: 'INTERSPEECH 2020',
+        time: '2020',
+        highlight: 'Oral'
        },
        {
         url: 'https://arxiv.org/pdf/1911.01799.pdf(Cn-Celeb)',
         title: 'Cn-celeb: a challenging chinese speaker recognition dataset',
         authers: ['Yue Fan', 'Jiawen Kang', 'Lantian Li', 'KC Li', 'HL Chen', 'ST Cheng', 'PY Zhang', 'ZY Zhou', 'YQ Cai', 'Dong Wang'],
         pub: 'INTERSPEECH 2020',
-        time: '2020'
+        time: '2020',
+        highlight: ''
        },
        {
         url: 'https://www.mdpi.com/1424-8220/18/12/4295',
         title: 'Mathematical Methods and Algorithms for Improving Near-Infrared Tunable Diode-Laser Absorption Spectroscopy',
         authers: ['Tianyu Zhang', 'Jiawen Kang', 'Dezhuang Meng', 'Hongwei Wang' , 'Zhengming Mu' , 'Meng Zhou', 'Xiaotong Zhang','Chen Chen'],
         pub: 'Sensors',
-        time: '2018'
+        time: '2018',
+        highlight: ''
        },
 
 
@@ -146,7 +162,7 @@ var outputHtml = function (publicationsArr) {
         }
         html.push(auther.join(', '));
         if (presenter == true){
-            html.push('. (*: presenter) </li>');
+            html.push('. (*: Presenter) </li>');
         } else {
             html.push('.</li>');
         }
@@ -154,6 +170,9 @@ var outputHtml = function (publicationsArr) {
         // html.push(publicationsArr[i].pub + publicationsArr[i].time);
         html.push('<p>');
         html.push(publicationsArr[i].pub);
+        if (publicationsArr[i].highlight != '') {
+            html.push(' <b>(' + publicationsArr[i].highlight + ')</b>');
+        }
         html.push('</p>');
         html.push('</ul>');
     }
