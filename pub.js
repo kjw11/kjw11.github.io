3537446f8e910674b02487f448abbcf9ae36594b
kjw11 <kangjw.mail@gmail.com>
2026-03-18 14:05:19 +0800

    Initial commit (history rewritten)

---
+++ pub.js
@@ -0,0 +1,333 @@
+
+var publications = [
+    {
+        url: 'https://www.arxiv.org/abs/2501.03727.pdf',
+        title: 'Detecting Neurocognitive Disorders through Analyses of Topic Evolution and Cross-modal Consistency in Visual-Stimulated Narratives',
+        authors: ['Jinchao Li^', 'Yuejiao Wang^', 'Junan Li^', 'Jiawen Kang^', 'Bo Zheng', 'Simon Wong', 'Brian Mak', 'Helene Fung', 'Jean Woo', 'Man-Wai Mak', 'Timothy Kwok', 'Vincent Mok', 'Xianmin Gong', 'Xixin Wu', 'Xunying Liu', 'Patrick Wong', 'Helen Meng'],
+        pub: 'Preprint',
+        time: '2025',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2409.16322.pdf',
+        title: 'Towards Within-Class Variation in Alzheimer\'s Disease Detection from Spontaneous Speech',
+        authors: ['Jiawen Kang', 'Dongrui Han', 'Lingwei Meng', 'Jingyan Zhou', 'Jinchao Li', 'Xixin Wu', 'Helen Meng'],
+        pub: 'Preprint',
+        time: '2024',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2409.12388.pdf',
+        title: 'Disentangling Speakers in Multi-Talker Speech Recognition with Speaker-Aware CTC',
+        authors: ['Jiawen Kang', 'Lingwei Meng', 'Mingyu Cui', 'Yuejiao Wang', 'Xixin Wu', 'Xunying Liu', 'Helen Meng'],
+        pub: 'Preprint',
+        time: '2024',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2412.06332.pdf',
+        title: 'Not All Errors Are Equal: Investigation of Speech Recognition Errors in Alzheimer’s Disease Detection',
+        authors: ['Jiawen Kang', 'Junan Li', 'Jinchao Li', 'Xixin Wu', 'Helen Meng'],
+        pub: 'IEEE ISCSLP 2024',
+        time: '2024',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2409.08596.pdf',
+        title: 'Large Language Model Can Transcribe Speech in Multi-Talker Scenarios with Versatile Instructions',
+        authors: ['Lingwei Meng', 'Shujie Hu', 'Jiawen Kang', 'Zhaoqing Li', 'Yuejiao Wang', 'Wenxuan Wu', 'Xixin Wu', 'Xunying Liu', 'Helen Meng'],
+        pub: 'Preprint',
+        time: '2024',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2409.08797.pdf',
+        title: 'Exploring SSL Discrete Speech Features for Zipformer-based Contextual ASR',
+        authors: ['Mingyu Cui', 'Yifan Yang', 'Jiajun Deng', 'Jiawen Kang', 'Shujie Hu', 'Tianzi Wang', 'Zhaoqing Li', 'Shiliang Zhang', 'Xie Chen', 'Xunying Liu'],
+        pub: 'Preprint',
+        time: '2024',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2407.09817.pdf',
+        title: 'Empowering whisper as a joint multi-talker and target-talker speech recognition system',
+        authors: ['Lingwei', 'Jiawen Kang', 'Yuejiao Wang', 'Zengrui Jin', 'Xixin Wu', 'Xunying Liu', 'Helen Meng'],
+        pub: 'INTERSPEECH 2024',
+        time: '2024',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2407.01850.pdf',
+        title: 'Purple-teaming LLMs with Adversarial Defender Training',
+        authors: ['Jingyan Zhou', 'Kun Li', 'Junan Li', 'Jiawen Kang', 'Minda Hu', 'Xixin Wu', 'Helen Meng'],
+        pub: 'Preprint',
+        time: '2024',
+        highlight: ''
+    },
+    {
+         url: 'https://arxiv.org/pdf/2401.04152.pdf',
+         title: 'Cross-Speaker Encoding Network for Multi-Talker Speech Recognition',
+         authors: ['Jiawen Kang', 'Lingwei Meng', 'Mingyu Cui', 'Haohan Guo', 'Xixin Wu', 'Xunying Liu', 'Helen Meng'],
+         pub: 'ICASSP 2024',
+         time: '2024',
+         highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2309.00126.pdf',
+        title: 'QS-TTS: Towards Semi-Supervised Text-to-Speech Synthesis via Vector-Quantized Self-Supervised Speech Representation Learning',
+        authors: ['Haohan Guo', 'Fenglong Xie', 'Jiawen Kang', 'Yujia Xiao', 'Xixin Wu', 'Helen Meng'],
+        pub: 'IEEE/ACM Transactions on Audio, Speech, and Language Processing',
+        time: '2024',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2306.13307.pdf',
+        title: 'Towards Effective and Compact Contextual Representation for Conformer Transducer Speech Recognition Systems',
+        authors: ['Mingyu Cui', 'Jiawen Kang', 'Jiajun Deng', 'Xi Yin', 'Yutao Xie', 'Xie Chen', 'Xunying Liu'],
+        pub: 'INTERSPEECH 2023',
+        time: '2023',
+        highlight: 'Oral'
+    },
+    {
+        url: 'https://www1.se.cuhk.edu.hk/~hccl/publications/pub/Lingwei%20Meng%20-%20Sidecar%20-%20update.pdf',
+        title: 'Unified Modeling of Multi-Talker Overlapped Speech Recognition and Diarization with a Sidecar Separator',
+        authors: ['Lingwei Meng', 'Jiawen Kang', 'Mingyu Cui', 'Haibin Wu', 'Xixin Wu', 'Helen Meng'],
+        pub: 'INTERSPEECH 2023',
+        time: '2023',
+        highlight: ''
+    },
+    {
+        url: 'https://www1.se.cuhk.edu.hk/~hccl/publications/pub/Lingwei%20Meng%20-%20Sidecar%20-%20update.pdf',
+        title: 'A Sidecar Separator Can Convert A Single-Talker Speech Recognition System to A Multi-Talker One',
+        authors: ['Lingwei Meng', 'Jiawen Kang', 'Mingyu Cui', 'Yuejiao Wang', 'Xixin Wu', 'Helen Meng'],
+        pub: 'ICASSP 2023',
+        time: '2023',
+        highlight: 'Oral'
+    },
+    {
+        url: 'https://arxiv.org/pdf/2305.12804.pdf',
+        title: 'The defender\'s perspective on automatic speaker verification: An overview',
+        authors: ['Haibin Wu', 'Jiawen Kang', 'Lingwei Meng', 'Helen Meng', 'Hung-yi Lee'],
+        pub: 'IJCAI DADA 2023',
+        time: '2023',
+        highlight: 'Oral',
+    },
+    {
+        url: 'https://www.isca-speech.org/archive/pdfs/interspeech_2023/meng23d_interspeech.pdf',
+        title: 'Integrated and enhanced pipeline system to support spoken language analytics for screening neurocognitive disorders',
+        authors: ['Helen Meng', 'Brian Mak', 'Man-Wai Mak', 'Helene Fung', 'Xianmin Gong', 'Timothy Kwok', 'Xunying Liu', 'Vincent Mok', 'Patrick Wong', 'Jean Woo', 'Xixin Wu', 'Ka Ho Wong', 'Sean Shensheng Xu', 'Naijun Zheng', 'Ranzo Huang', 'Jiawen Kang*', 'Xiaoquan Ke', 'Junan Li', 'Jinchao Li', 'Yi Wang'],
+        pub: 'INTERSPEECH 2023',
+        time: '2023',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2206.09131.pdf',
+        title: 'Tackling spoofing-aware speaker verification with multi-model fusion',
+        authors: ['Haibin Wu', 'Jiawen Kang', 'Lingwei Meng', 'Yang Zhang', 'Xixin Wu', 'Zhiyong Wu', 'Hung-yi Lee', 'Helen Meng'],
+        pub: 'Odyssey 2022',
+        time: '2022',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2202.01986.pdf',
+        title: 'The cuhk-tencent speaker diarization system for the icassp 2022 multi-channel multi-party meeting transcription challenge',
+        authors: ['Naijun Zheng', 'Na Li', 'Xixin Wu', 'Lingwei Meng', 'Jiawen Kang', 'Haibin Wu', 'Chao Weng', 'Dan Su', 'Helen Meng'],
+        pub: 'ICASSP 2022',
+        time: '2022',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2202.08216.pdf',
+        title: 'TalkTive: a conversational agent using backchannels to engage older adults in neurocognitive disorders screening',
+        authors: ['Zijian Ding', 'Jiawen Kang', 'Tinky Oi Ting Ho', 'Ka Ho Wong', 'Helene H Fung', 'Helen Meng', 'Xiaojuan Ma'],
+        pub: 'CHI 2022',
+        time: '2022',
+        highlight: 'Oral'
+    },
+    {
+        url: 'https://arxiv.org/pdf/2203.15377.pdf',
+        title: 'Spoofing-Aware Speaker Verification by Multi-Level Fusion',
+        authors: ['Haibin Wu', 'Lingwei Meng', 'Jiawen Kang', 'Jinchao Li', 'Xu Li', 'Xixin Wu', 'Hung-yi Lee', 'Helen Meng'],
+        pub: 'INTERSPEECH 2022',
+        time: '2022',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2012.12471.pdf',
+        title: 'A principle solution for enroll-test mismatch in speaker recognition',
+        authors: ['Lantian Li', 'Dong Wang', 'Jiawen Kang', 'Renyu Wang', 'Jing Wu', 'Zhendong Gao', 'Xiao Chen'],
+        pub: 'IEEE/ACM Transactions on Audio, Speech, and Language Processing',
+        time: '2021',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2012.12468.pdf',
+        title: 'Cn-celeb: multi-genre speaker recognition',
+        authors: ['Lantian Li^', 'Ruiqi Liu^', 'Jiawen Kang^', 'Yue Fan', 'Hao Cui', 'Yunqi Cai', 'Ravichander Vipperla', 'Thomas Fang Zheng', 'Dong Wang'],
+        pub: 'Speech Communication',
+        time: '2021',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2010.14243.pdf',
+        title: 'Squeezing value of cross-domain labels: A decoupled scoring approach for speaker verification',
+        authors: ['Lantian Li', 'Yang Zhang', 'Jiawen Kang', 'Thomas Fang Zheng', 'Dong Wang'],
+        pub: 'ICASSP 2021',
+        time: '2021',
+        highlight: ''
+    },
+    {
+        url: 'https://arxiv.org/pdf/2005.11900.pdf',
+        title: 'Domain-invariant speaker vector projection by model-agnostic meta-learning',
+        authors: ['Jiawen Kang', 'Ruiqi Liu', 'Lantian Li', 'Yunqi Cai', 'Dong Wang', 'Thomas Fang Zheng'],
+        pub: 'INTERSPEECH 2020',
+        time: '2020',
+        highlight: 'Oral'
+    },
+    {
+        url: 'https://arxiv.org/pdf/1911.01799.pdf(Cn-Celeb)',
+        title: 'Cn-celeb: a challenging chinese speaker recognition dataset',
+        authors: ['Yue Fan', 'Jiawen Kang', 'Lantian Li', 'KC Li', 'HL Chen', 'ST Cheng', 'PY Zhang', 'ZY Zhou', 'YQ Cai', 'Dong Wang'],
+        pub: 'INTERSPEECH 2020',
+        time: '2020',
+        highlight: ''
+    },
+    {
+        url: 'https://www.mdpi.com/1424-8220/18/12/4295',
+        title: 'Mathematical Methods and Algorithms for Improving Near-Infrared Tunable Diode-Laser Absorption Spectroscopy',
+        authors: ['Tianyu Zhang', 'Jiawen Kang', 'Dezhuang Meng', 'Hongwei Wang' , 'Zhengming Mu' , 'Meng Zhou', 'Xiaotong Zhang','Chen Chen'],
+        pub: 'Sensors',
+        time: '2018',
+        highlight: ''
+    },
+
+];
+
+var filter = function (year) {
+    var final = [];
+    for (var i = 0; i < publications.length; i++) {
+        if (publications[i].time == year) {
+            final.push(publications[i]);
+        }
+    }
+    return final;
+};
+
+
+var outputHtml = function (publicationsArr) {
+
+    // if empty
+    if (publicationsArr.length == 0) {
+        return [];
+    }
+
+    var html = [];
+    html.push('<ul>');
+    for (var i = 0; i < publicationsArr.length; i++) {
+
+        html.push('<li>');
+        html.push('<p>');
+        html.push('<a href="' + publicationsArr[i].url + '" target="_blank">' + publicationsArr[i].title + '</a>');
+        html.push('</p>');
+
+        // loop authors
+        html.push('<p>');
+        var auther = [];
+        for (var j = 0; j < publicationsArr[i].authors.length; j++) {
+            if (publicationsArr[i].authors[j] == 'Jiawen Kang') {
+                auther.push('<u>Jiawen Kang</u>');
+                // auther.push('Jiawen Kang');
+            } else if (publicationsArr[i].authors[j] == 'Jiawen Kang*') {
+               auther.push('<u>Jiawen Kang*</u>');
+            //    auther.push('Jiawen Kang*');
+               var presenter = true
+            } else if (publicationsArr[i].authors[j] == 'Jiawen Kang^') {
+                auther.push('<u>Jiawen Kang^</u>');
+                var equal_contributor = true
+            } else {
+                auther.push(publicationsArr[i].authors[j]);
+            }
+        }
+
+        html.push(auther.join(', '));
+        if (presenter == true){
+            html.push('. (*: Presenter)');
+        } else if (equal_contributor == true){
+            html.push('. (^: Equal Contribution)');
+        } else {
+            html.push('.');
+        }
+        html.push('</p>');
+
+        html.push('<p>');
+        html.push(publicationsArr[i].pub);
+        if (publicationsArr[i].highlight != '') {
+            html.push(' <b>(' + publicationsArr[i].highlight + ')</b>');
+        }
+        html.push('</p>');
+
+        html.push('</li>');
+    }
+    html.push('</ul>');
+    return html;
+};
+
+
+var listPub = function (year, urlPart, format) {
+    var container = document.getElementById('publications');
+    container.innerHTML = ''; // clear
+
+
+    // var publicationsArr = filter(year);
+    // html = outputHtml(publicationsArr);
+    // container.innerHTML += html.join('');
+
+
+    // inset html "<p style="color: #991D2E; font-weight: bold;"> <em>recent papers</em></p>"
+    container.innerHTML += '<br><br> <p style="color: #991D2E; font-weight: bold;"> <em> list of all papers </em></p>';
+
+    // loop all years Now - 2018
+    for (var i = year; i >= 2018; i--) {
+        
+        var publicationsArr = filter(i);
+        html = outputHtml(publicationsArr);
+        if (html.length > 0) {
+            container.innerHTML += '<h4>' + i + '</h4>';
+        }
+        container.innerHTML += html.join('');
+    }
+
+    // if (urlPart.endsWith('/papers.html')) {
+
+    //     // loop all years Now - 2018
+    //     for (var i = year; i >= 2018; i--) {
+            
+    //         var publicationsArr = filter(i);
+    //         html = outputHtml(publicationsArr);
+    //         if (html.length > 0) {
+    //             container.innerHTML += '<h3>' + i + '</h3>';
+    //         }
+    //         container.innerHTML += html.join('');
+    //     }
+    // } else {
+
+    //     var publicationsArr = filter(year);
+    //     html = outputHtml(publicationsArr);
+    //     container.innerHTML += html.join('');
+        
+    // }
+};
+
+var selYear = function () {
+    var selector = document.getElementById('select-year');
+    var year = selector.value;
+    var urlPart = window.location.pathname;
+    listPub(year, urlPart)
+};
+
+window.onload = function () {
+    var year = new Date().getFullYear();
+    var urlPart = window.location.pathname;
+    listPub(year, urlPart);  
+
+};
